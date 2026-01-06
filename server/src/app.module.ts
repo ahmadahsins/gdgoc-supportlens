3b5bf69cda54e9e1f91d7cc6d3418787ec8a0cde
Ahmad Ahsin Syamil <118179124+ahmadahsins@users.noreply.github.com>
2026-01-06 17:40:04 +0700

    feat: implement firebase auth guard and user sync endpoint (#3)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,13 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { FirebaseModule } from './firebase/firebase.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     FirebaseModule,
+    AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
---
+++ server/src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { FirebaseAuthGuard } from './guards/firebase-auth/firebase-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) { }
+  
+  @Post('sync')
+  @UseGuards(FirebaseAuthGuard)
+  async sync(@Req() req) {
+    return this.authService.syncUser(req.user);
+  }
+}
+
